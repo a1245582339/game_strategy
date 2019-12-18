cfec479b996e4355375e7a85235c2081361cde55
a1245582339 <1245582339@qq.com>
2019-12-18 23:29:03 +0800

    feat: 左右值添加与删除

--- server/src/Category/category.controller.ts
+++ server/src/Category/category.controller.ts
@@ -31,4 +31,40 @@ export class CategoryController {
             throw err
         }
     }
+
+    @Put('create/:id')
+    async update(@Res() res: Response, @Body('name') name: string, @Param('id', new ParseIntPipe()) id:number) {
+        try {
+            if (await this.categoryService.update(id, name)) {
+                res.json({
+                    msg: 'Ok'
+                })
+            } else {
+                res.json({
+                    code: 20004,
+                    msg: `No found category as id = ${id}`
+                })
+            }
+        } catch (err) {
+            throw err
+        }
+    }
+
+    @Put('del/:id')
+    async delete(@Param('id', new ParseIntPipe()) id:number, @Res() res: Response) {
+        try {
+            if (await this.categoryService.delete(id)) {
+                res.json({
+                    msg: 'Ok'
+                })
+            } else {
+                res.json({
+                    code: 20004,
+                    msg: `No found category as id = ${id}`
+                })
+            }
+        } catch (err) {
+            throw err
+        }
+    }
 }
--- server/src/Category/category.service.ts
+++ server/src/Category/category.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common'
 import { InjectRepository } from '@nestjs/typeorm'
-import { Repository, Between, LessThan } from 'typeorm'
+import { Repository, Between, LessThan, getConnection } from 'typeorm'
 import { Category } from './category.entity'
 import { CategoryDto } from './category.dto'
 
@@ -16,7 +16,7 @@ export class CategoryService {
             type: LessThan(3)
         })
     }
-    async create(body: CategoryDto) {
+    async create(body: CategoryDto): Promise<boolean> {
         const { p_id } = body
         const parent = await this.categoryService.findOne({ id: p_id , is_del: 0 })
         if (!parent) {
@@ -26,8 +26,54 @@ export class CategoryService {
         const l_id = parent.r_id
         const r_id = l_id + 1
         try {
+            const updateRid = this.categoryService.createQueryBuilder().update(Category).set({
+                r_id: () => "r_id + 2",
+            }).where(
+                'r_id>=:r_id and is_del=0', {r_id: r_id - 1}
+            ).execute()
+            const updateLid = this.categoryService.createQueryBuilder().update(Category).set({
+                l_id: () => "l_id + 2",
+            }).where(
+                'l_id>=:l_id and is_del=0', {l_id}
+            ).execute()
+            await Promise.all([updateRid, updateLid])
             const create = await this.categoryService.save({...body, type, l_id, r_id})
             return !!create
+        } catch (err) {
+            throw err
+        }
+    }
+    async update(id: number, name: string): Promise<boolean> {
+        try {
+            const update = await this.categoryService.update({
+                id, is_del: 0
+            }, { name })
+            return !!update.raw.changedRows
+        } catch (err) {
+            throw err
+        }
+    }
+    async delete(id: number) {
+        try {
+            const catecory = await this.categoryService.findOne({ id , is_del: 0 })
+            if (!catecory.p_id) return false
+            if (!catecory) return false
+            const { l_id, r_id } = catecory
+            await this.categoryService.createQueryBuilder().delete().from(Category).where(
+                'l_id>=:l_id and r_id<=:r_id and is_del=0', { l_id, r_id }
+            ).execute()
+            const updateRid = this.categoryService.createQueryBuilder().update(Category).set({
+                r_id: () => `r_id-(${r_id-l_id+1})`
+            }).where(
+                'r_id>=:r_id and is_del=0', { r_id }
+            ).execute()
+            const updateLid = this.categoryService.createQueryBuilder().update(Category).set({
+                l_id: () => `l_id-(${r_id-l_id+1})`
+            }).where(
+                'l_id>=:l_id and is_del=0', { l_id }
+            ).execute()
+            await Promise.all([updateRid, updateLid])
+            return true
         } catch (err) {
             throw err
         }
