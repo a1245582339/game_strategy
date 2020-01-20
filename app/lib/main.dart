c7505326b4511e2dec489367bea38bb097720747
李志鹏 <lizhipeng@haodf.com>
2020-01-20 16:52:14 +0800

    feat: 登录与退出登录

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:app/component/appBar/home.dart';
 import 'package:app/component/appBar/notice.dart';
 import 'package:app/component/appBar/category.dart';
+import 'package:app/page/setting/index.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:bot_toast/bot_toast.dart';
@@ -9,6 +10,7 @@ import './page/home/index.dart';
 import './page/notice/index.dart';
 import './page/category/index.dart';
 import './utils/userInfo.dart';
+import 'page/login/login.dart';
 
 void main() => runApp(MyApp());
 
@@ -51,13 +53,13 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   int _currentIndex = 0;
   List<Widget> pages = [];
-    List<Widget> appBar = [];
+  List<Widget> appBar = [];
   @override
   void initState() {
     super.initState();
     appBar..add(HomeBar())..add(CategoryBar())..add(NoticeBar());
     pages..add(Home())..add(Category())..add(Notice());
-    
+
     UserInfo(context);
   }
 
@@ -89,16 +91,92 @@ class _MyHomePageState extends State<MyHomePage> {
                     accountName: Text(Provider.of<Store>(context)
                             .getUserInfo['nick_name'] ??
                         Provider.of<Store>(context).getUserInfo['login_name']),
-                    accountEmail: Text('123123'),
+                    accountEmail:
+                        Text(Provider.of<Store>(context).getUserInfo['email']),
                     currentAccountPicture: CircleAvatar(
                       child: Text('R'),
                     ),
                   )
-                : Text('123'),
+                : DrawerHeader(
+                    child: Padding(
+                      padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text(
+                            '登录后有新天地',
+                            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                          ),
+                          Text('让你体验私人定制', style: TextStyle(color: Color(0xFF666666))),
+                          SizedBox(
+                              height: 10,
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: <Widget>[
+                              Container(
+                                width: 110,
+                                child: OutlineButton(
+                                  child: Text('注册'),
+                                  textColor: Theme.of(context).primaryColor,
+                                  borderSide: BorderSide(
+                                      color: Theme.of(context).primaryColor,
+                                      width: 1),
+                                  shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(5)),
+                                  onPressed: () {},
+                                ),
+                              ),
+                              Container(
+                                width: 110,
+                                child: RaisedButton(
+                                  child: Text('登录'),
+                                  textColor: Colors.white,
+                                  color: Theme.of(context).primaryColor,
+                                  disabledColor:
+                                      Theme.of(context).primaryColorLight,
+                                  disabledTextColor: Colors.white,
+                                  shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(5)),
+                                  onPressed: () {
+                                      Navigator.of(context).pop();
+                                      Navigator.of(context).push(MaterialPageRoute(builder: (context) {
+                                        return Login();
+                                    }));
+                                  },
+                                ),
+                              )
+                            ],
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+            ListTile(
+              leading: Icon(Icons.star_border),
+              title: Text('我的收藏'),
+              onTap: () {
+
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.notifications_none),
+              title: Text('我的关注'),
+              onTap: () {
+                  
+              },
+            ),
             ListTile(
               leading: Icon(Icons.settings),
               title: Text('设置'),
-            )
+              onTap: () {
+                  Navigator.of(context).pop();
+                  Navigator.of(context).push(MaterialPageRoute(builder: (context) {
+                      return Setting();
+                  }));
+              },
+            ),
           ],
         ),
       ),
---
+++ app/lib/page/setting/index.dart
@@ -0,0 +1,64 @@
+import 'package:app/store/index.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class Setting extends StatelessWidget {
+  const Setting({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('设置'),
+      ),
+      body: Container(
+        padding: EdgeInsets.all(10),
+        child: ListView(
+          children: <Widget>[
+            Offstage(
+              offstage: Provider.of<Store>(context).getUserInfo['id'] == null,
+              child: RaisedButton(
+                color: Color(0xFFFDFDFD),
+                child: Text('退出登录'),
+                onPressed: () {
+                  showDialog(
+                      context: context,
+                      barrierDismissible: false,
+                      builder: (BuildContext context) {
+                        return AlertDialog(
+                          title: Text('确定退出登录？'),
+                          content: SingleChildScrollView(
+                            child: ListBody(
+                              children: <Widget>[
+                                Text('退出后收藏内容与关注依然会保留'),
+                              ],
+                            ),
+                          ),
+                          actions: <Widget>[
+                            FlatButton(
+                                onPressed: () async {
+                                  Navigator.of(context).pop();
+                                  Provider.of<Store>(context, listen: false).clearUserInfo();
+                                  SharedPreferences prefs =
+                                      await SharedPreferences.getInstance();
+                                  await prefs.setString('token', '');
+                                },
+                                child: Text('确定')),
+                            FlatButton(
+                                onPressed: () {
+                                  Navigator.of(context).pop();
+                                },
+                                child: Text('取消')),
+                          ],
+                        );
+                      });
+                },
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
