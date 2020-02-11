c89de7c3ed93dec0c0ac4455854ecc19f15b56bf
a1245582339 <1245582339@qq.com>
2020-02-11 12:10:15 +0800

    feat: 删除状态时返回不退出页面只关闭状态

--- app/lib/page/favorites/index.dart
+++ app/lib/page/favorites/index.dart
@@ -67,126 +67,137 @@ class _FavoritesState extends State<Favorites> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-          title: Text('我的收藏'),
-          actions: _delFlag
-              ? <Widget>[
-                  IconButton(
-                      icon: Icon(Icons.delete),
-                      onPressed: () {
-                        if (_checkedIds.length == 0) {
-                          BotToast.showText(text: '请选择后点击删除');
-                          return;
-                        }
-                        showDialog(
-                            context: context,
-                            builder: (BuildContext context) {
-                              return AlertDialog(
-                                title: Text('确定删除这${_checkedIds.length}个收藏？'),
-                                actions: <Widget>[
-                                  FlatButton(
-                                      onPressed: () async {
-                                        await Http().put('/favorites/delByid',
-                                            body: {
-                                              'ids': jsonEncode(_checkedIds)
-                                            },
-                                            auth: true);
-                                        Navigator.of(context).pop();
-                                        setState(() {
-                                          _page = 0;
-                                          _data = [];
-                                          _favoritesList = [Loadmore()];
-                                        });
-                                        _getData();
-                                      },
-                                      child: Text('确定')),
-                                  FlatButton(
-                                      onPressed: () {
-                                        Navigator.of(context).pop();
-                                      },
-                                      child: Text('取消')),
-                                ],
-                              );
-                            });
-                      }),
-                  IconButton(
-                      icon: Icon(Icons.close),
-                      onPressed: () {
-                        _changeModel(false);
-                      })
-                ]
-              : null),
-      body: Container(
-          child: ListView(
-        children: _data.map<Widget>((item) {
-          return Column(
-            children: <Widget>[
-              Row(children: <Widget>[
-                Offstage(
-                  offstage: !_delFlag,
-                  child: Checkbox(
-                      value: _checkedIds.contains(item['id']),
-                      onChanged: (state) {
-                        if (state) {
-                          setState(() {
-                            _checkedIds.add(item['id']);
-                          });
-                        } else {
-                          setState(() {
-                            _checkedIds.remove(item['id']);
-                          });
-                        }
-                      }),
-                ),
-                Expanded(
-                  child: ListTile(
-                    title: Text(
-                      item['article']['title'],
-                      maxLines: 2,
-                      overflow: TextOverflow.ellipsis,
-                    ),
-                    leading: Image.network(
-                      item['article']['cover'],
-                      width: 100,
-                      fit: BoxFit.cover,
-                    ),
-                    trailing: !_delFlag ? Icon(Icons.chevron_right) : null,
-                    onTap: _delFlag
-                        ? () {
-                            if (_checkedIds.contains(item['id'])) {
-                              setState(() {
-                                _checkedIds.remove(item['id']);
-                              });
-                            } else {
+    return WillPopScope(
+        child: Scaffold(
+          appBar: AppBar(
+              title: Text('我的收藏'),
+              actions: _delFlag
+                  ? <Widget>[
+                      IconButton(
+                          icon: Icon(Icons.delete),
+                          onPressed: () {
+                            if (_checkedIds.length == 0) {
+                              BotToast.showText(text: '请选择后点击删除');
+                              return;
+                            }
+                            showDialog(
+                                context: context,
+                                builder: (BuildContext context) {
+                                  return AlertDialog(
+                                    title:
+                                        Text('确定删除这${_checkedIds.length}个收藏？'),
+                                    actions: <Widget>[
+                                      FlatButton(
+                                          onPressed: () async {
+                                            await Http().put(
+                                                '/favorites/delByid',
+                                                body: {
+                                                  'ids': jsonEncode(_checkedIds)
+                                                },
+                                                auth: true);
+                                            Navigator.of(context).pop();
+                                            setState(() {
+                                              _page = 0;
+                                              _data = [];
+                                              _favoritesList = [Loadmore()];
+                                            });
+                                            _getData();
+                                          },
+                                          child: Text('确定')),
+                                      FlatButton(
+                                          onPressed: () {
+                                            Navigator.of(context).pop();
+                                          },
+                                          child: Text('取消')),
+                                    ],
+                                  );
+                                });
+                          }),
+                      IconButton(
+                          icon: Icon(Icons.close),
+                          onPressed: () {
+                            _changeModel(false);
+                          })
+                    ]
+                  : null),
+          body: Container(
+              child: ListView(
+            children: _data.map<Widget>((item) {
+              return Column(
+                children: <Widget>[
+                  Row(children: <Widget>[
+                    Offstage(
+                      offstage: !_delFlag,
+                      child: Checkbox(
+                          value: _checkedIds.contains(item['id']),
+                          onChanged: (state) {
+                            if (state) {
                               setState(() {
                                 _checkedIds.add(item['id']);
                               });
+                            } else {
+                              setState(() {
+                                _checkedIds.remove(item['id']);
+                              });
                             }
-                          }
-                        : () {
-                            Future.delayed(Duration(milliseconds: 200), () {
-                              Navigator.of(context)
-                                  .push(MaterialPageRoute(builder: (context) {
-                                return Article(id: item['article']['id']);
-                              }));
-                            });
-                          },
-                    onLongPress: () {
-                      _checkedIds.add(item['id']);
-                      _changeModel(true);
-                    },
-                    contentPadding: EdgeInsets.fromLTRB(16, 10, 16, 10),
-                  ),
-                ),
-                Divider(
-                  height: 1,
-                )
-              ])
-            ],
-          );
-        }).toList(),
-      )),
-    );
+                          }),
+                    ),
+                    Expanded(
+                      child: ListTile(
+                        title: Text(
+                          item['article']['title'],
+                          maxLines: 2,
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                        leading: Image.network(
+                          item['article']['cover'],
+                          width: 100,
+                          fit: BoxFit.cover,
+                        ),
+                        trailing: !_delFlag ? Icon(Icons.chevron_right) : null,
+                        onTap: _delFlag
+                            ? () {
+                                if (_checkedIds.contains(item['id'])) {
+                                  setState(() {
+                                    _checkedIds.remove(item['id']);
+                                  });
+                                } else {
+                                  setState(() {
+                                    _checkedIds.add(item['id']);
+                                  });
+                                }
+                              }
+                            : () {
+                                Future.delayed(Duration(milliseconds: 200), () {
+                                  Navigator.of(context).push(
+                                      MaterialPageRoute(builder: (context) {
+                                    return Article(id: item['article']['id']);
+                                  }));
+                                });
+                              },
+                        onLongPress: () {
+                          _checkedIds.add(item['id']);
+                          _changeModel(true);
+                        },
+                        contentPadding: EdgeInsets.fromLTRB(16, 10, 16, 10),
+                      ),
+                    ),
+                    Divider(
+                      height: 1,
+                    )
+                  ])
+                ],
+              );
+            }).toList(),
+          )),
+        ),
+        onWillPop: () {
+          if (_delFlag) { 
+            _changeModel(false);
+            return Future.value(false);
+          } else {
+            return Future.value(true);
+          }
+        });
   }
 }
